ddfd4102faeadd73ad8c85709d7249447dd8c8c9
DK <daniasadiaa@gmail.com>
2026-07-24 16:01:45 +0200

    Stage 2: read endpoints with 404

--- W2 · A1 — Build your first CRUD API/api.py
+++ W2 · A1 — Build your first CRUD API/api.py
@@ -1,6 +1,11 @@
 import fastapi
 app = fastapi.FastAPI()
-
+task_list = [
+   { "id": "1", "title": "First task.", "Done": "True" },
+   { "id": "2", "title": "First task.", "Done": "True" },
+   { "id": "3", "title": "Third task.", "Done": "False"}
+]
+      
 # Stage 0: hello server
 # @app.get("/")
 # async def hello_server():
@@ -11,3 +16,14 @@ async def root():
 @app.get("/health")
 async def health():
    return { "status": "ok" } 
+@app.get("/tasks")
+async def tasks():
+   return task_list
+@app.get("/tasks/{id}")
+async def get_task(id:str):
+     for task in task_list:
+         if task["id"] == id:
+             return task
+         else:
+             return { "error": f"Task {id} not found" }
+     
--- W2 · A1 — Build your first CRUD API/frontend.py
+++ W2 · A1 — Build your first CRUD API/frontend.py
@@ -5,6 +5,14 @@ button1 = sl.button("Call the root API")
 url = "http://127.0.0.1:8000"
 if button1:
        sl.write(rq.get(url).json())
-button2 = sl.button("Call the health API.")
-if button2:
+health_button = sl.button("Call the health API.")
+if health_button:
        sl.write(rq.get(url + "/health").json())
+tasks_button = sl.button("Get all lists.")
+if tasks_button:
+       sl.write(rq.get(url + "/tasks").json())
+get_task = sl.text_input("Enter list id.")
+task_button = sl.button("Get an item from the list using the item's id.")
+if (get_task != " "):
+       if task_button:
+              sl.write(rq.get(url + "/tasks/" + get_task).json())
